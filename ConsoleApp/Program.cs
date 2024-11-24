6160315f18213664e413a164395cec41513ab45d
Michael Ludwig <mludwig2@css.edu>
2024-11-24 15:15:26 -0600

    Assignment 12 Completion

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,12 +5,17 @@ class Program
     static void Main(string[] args)
     {
 
-        EntityRepository entityRepository = new EntityRepository();
+        IEntityRepository<Entity> entityRepository = new EntityRepository();
+        IEntityRepository<User> userRepository = new UserRepository();
         Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
+        User myUser = userRepository.GetByID(Guid.NewGuid());
 
         myEntity.Name = "Tom";
+        myUser.Name = "Jerry";
         Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
         Console.WriteLine("New entity Name: " + myEntity.Name);
+        Console.WriteLine("New user ID: " + myUser.ID.ToString());
+        Console.WriteLine("New user Name: " + myUser.Name);
 
     }
 }
